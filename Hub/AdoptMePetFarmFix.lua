857a49c5ab38d037ff1d406597a7c5b5c761527b
ReQiuYTPL <56265894+ReQiuYTPL@users.noreply.github.com>
2026-02-25 14:13:14 +0100

    Update AdoptMePetFarmFix.lua

--- Hub/AdoptMePetFarmFix.lua
+++ Hub/AdoptMePetFarmFix.lua
@@ -1,17 +1,35 @@
+-- if not _G.hola then
+--   _G.hola = true
+--   task.wait(10)
+--   if _G.savingElements["toggles"]['petautofarm'].CurrentValue then
+--       _G.savingElements["toggles"]['petautofarm']:Set(false)
+--       task.wait(5)
+--       _G.savingElements["toggles"]['petautofarm']:Set(true)
+--   end
+--   while true do
+--     task.wait(360)
+--     if _G.savingElements["toggles"]['petautofarm'].CurrentValue then
+--       _G.savingElements["toggles"]['petautofarm']:Set(false)
+--       task.wait(10)
+--       _G.savingElements["toggles"]['petautofarm']:Set(true)
+--     end
+--   end
+-- end
+
 if not _G.hola then
   _G.hola = true
   task.wait(10)
-  if _G.savingElements["toggles"]['petautofarm'].CurrentValue then
-      _G.savingElements["toggles"]['petautofarm']:Set(false)
+  if _G.autofarm then
+      _G.autofarm = false
       task.wait(5)
-      _G.savingElements["toggles"]['petautofarm']:Set(true)
+      _G.autofarm = true
   end
   while true do
     task.wait(360)
-    if _G.savingElements["toggles"]['petautofarm'].CurrentValue then
-      _G.savingElements["toggles"]['petautofarm']:Set(false)
+    if _G.autofarm then
+      _G.autofarm = false
       task.wait(10)
-      _G.savingElements["toggles"]['petautofarm']:Set(true)
+      _G.autofarm = true
     end
   end
 end
